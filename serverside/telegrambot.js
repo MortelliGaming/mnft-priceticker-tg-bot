068e315e5e5e493180181cfbbe116dc585562dc5
unknown <mortelligaming@gmail.com>
2022-01-21 12:24:27 +0100

    added some more info commands

--- serverside/telegrambot.js
+++ serverside/telegrambot.js
@@ -1,5 +1,5 @@
 
-const { Telegraf } = require('telegraf')
+const { Telegraf, Markup } = require('telegraf')
 const puppeteer = require('puppeteer-extra')
 
 require('dotenv').config()
@@ -39,6 +39,116 @@ initializeBot = function() {
             if(!found)
                 ctx.reply(getErrorMessage())
         })
+    })
+
+    bot.command("contract", (ctx) => {
+        // ask bsc/polyon(matic)/velas
+        var keyData =  ['BSC', 'Polygon', 'Velas'].map(item => {
+            return {
+                callback_data: 'contract_' + item,
+                text: item,
+            }
+        })
+        ctx.reply(`on which blockchain?`, { reply_markup: {
+            inline_keyboard: [keyData],
+        }});
+    })
+    bot.command("trade", (ctx) => {
+        // ask bsc/polyon(matic)/velas/cex
+        var keyData =  ['BSC', 'Polygon', 'Velas', 'CEX'].map(item => {
+            return {
+                callback_data: 'trade_' + item,
+                text: item,
+            }
+        })
+        ctx.reply(`on which blockchain?`, { reply_markup: {
+            inline_keyboard: [keyData],
+        }});
+        // pancake link
+        // quickswap link
+        // velas bridge link
+        // ascendex link
+    })
+    bot.command("token", (ctx) => {
+        // ask cmc / coingecko / dextools
+        var keyData =  ['CMC', 'Coingecko', 'DexTools'].map(item => {
+            return {
+                callback_data: 'token_' + item,
+                text: item,
+            }
+        })
+        ctx.reply(`where you get your infos from?`, { reply_markup: {
+            inline_keyboard: [keyData],
+        }});
+        // cmc link
+        // coingecko link
+        // ask bsc / polygon
+    })
+
+    bot.on('callback_query', (ctx) => {
+        switch(ctx.callbackQuery.data.toLowerCase()) {
+            case 'contract_bsc':
+                ctx.reply('*BSC Contract* \n 0x33BE7644c0E489b3A0c639D103392D4F3e338158', { parse_mode: "Markdown" })
+                ctx.deleteMessage();
+                break;
+            case 'contract_polygon':
+                ctx.reply('*Polygon (Matic) Contract* \n 0xd281aF594Cbb99E8469f3591D57A0C72EB725bdB', { parse_mode: "Markdown" })
+                ctx.deleteMessage();
+                break;
+            case 'contract_velas':
+                ctx.reply('*Velas Contract* \n 0x4cBA3447E51239065310E24c02C190945ad761d9', { parse_mode: "Markdown" })
+                ctx.deleteMessage();
+                break;
+            case 'trade_bsc':
+                ctx.reply('*PancakeSwap* \n [Swap on Pancake](https://pancakeswap.finance/swap?outputCurrency=0x33BE7644c0E489b3A0c639D103392D4F3e338158)', { parse_mode: "Markdown", disable_web_page_preview: true })
+                ctx.deleteMessage();
+                break;
+            case 'trade_polygon':
+                ctx.reply('*QuickSwap* \n [Swap on Quickswap](https://quickswap.exchange/#/swap?outputCurrency=0xd281aF594Cbb99E8469f3591D57A0C72EB725bdB)', { parse_mode: "Markdown", disable_web_page_preview: true })
+                ctx.deleteMessage();
+                break;
+            case 'trade_velas':
+                ctx.reply('*Bridge To BSC* \n [VelasPad Bridge](https://bridge.velaspad.io/#contract)', { parse_mode: "Markdown", disable_web_page_preview: true })
+                ctx.deleteMessage();
+                break;
+            case 'trade_cex':
+                ctx.reply('*Ascendex* \n [Trade on Ascendex](https://ascendex.com/en/basic/cashtrade-spottrading/usdt/mnft)', { parse_mode: "Markdown", disable_web_page_preview: true })
+                ctx.deleteMessage();
+                break;
+            case 'token_cmc':
+                ctx.reply('*CoinMarketCap* \n [Go To CoinMarketCap](https://coinmarketcap.com/currencies/marvelous-nfts-bad-days)', { parse_mode: "Markdown", disable_web_page_preview: true })
+                ctx.deleteMessage();
+                break;
+            case 'token_coingecko':
+                ctx.reply('*coingecko* \n [Go To coingecko](https://www.coingecko.com/en/coins/marvelous-nfts)', { parse_mode: "Markdown", disable_web_page_preview: true })
+                ctx.deleteMessage();
+                break;
+            case 'token_dextools':
+                var keyData =  ['BSC', 'Polygon'].map(item => {
+                    return {
+                        callback_data: 'token_dextools_' + item,
+                        text: item,
+                    }
+                })
+                ctx.reply(`on which blockchain?`, { reply_markup: {
+                    inline_keyboard: [keyData],
+                }});
+                ctx.deleteMessage();
+                break;
+            case 'token_dextools_bsc':
+                ctx.reply('*dextools bsc pairs* \n [Go To dextools](https://www.dextools.io/app/bsc/pair-explorer/0xb3bb53f873c8fc59f0e62580b1b35802bb5688ce)', { parse_mode: "Markdown", disable_web_page_preview: true })
+                ctx.deleteMessage();
+                break;
+            case 'token_dextools_polygon':
+                ctx.reply('*dextools polygon pairs* \n [Go To dextools](https://www.dextools.io/app/polygon/pair-explorer/0x0463302f0f9f847d2c6164525e0d966a9bdce71c)', { parse_mode: "Markdown", disable_web_page_preview: true })
+                ctx.deleteMessage();
+                break;
+            default:
+                break;
+        }
+        ctx.telegram.answerCbQuery(ctx.callbackQuery.id)
+        // Using context shortcut
+        ctx.answerCbQuery()
     })
 
     bot.launch()
